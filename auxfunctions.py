a479cee28c90be64a93cac3d58ecfb7e68c26e0a
LipeAma <contato.feamaral@gmail.com>
2024-11-25 15:17:57 -0300

    inv_human function

--- auxfunctions.py
+++ auxfunctions.py
@@ -4,34 +4,8 @@ import requests
 from functools import partial
 from numpy import array
 
-def int2hotvec(intlist, vocab):
-  return [[1 if j == val else 0 for j in range(len(vocab))] for val in intlist]
-
-def phrase2int(string, length, vocab):
-    string = string.replace(',', '')
-    string = string[:length]
-    string = string.lower()
-    unk = vocab['<unk>']
-    pad = vocab['<pad>']
-
-    int_list = [vocab.get(x, unk) for x in string]
-    padded_list = int_list + [pad] * (length - len(string))
-
-    return padded_list
-
-def phrase2hotvec(string, length, vocab):
-    return array(int2hotvec(phrase2int(string, length, vocab), vocab))
-
-def date2int(string, vocab):
-    return [vocab[x] for x in string]
-
-def date2hotvec(string, vocab):
-    return int2hotvec(date2int(string, vocab), vocab)
-
-def hotvec2phrase(hotvec, vocab):
-    return ''.join([vocab[str(k)] for k in [n.argmax() for n in hotvec]])
-
-files = ["human_vocab.json", "machine_vocab.json", "dataset.json", "inv_machine_vocab.json"]
+###################### Download dos vocabulários e do dataset ######################
+files = ["human_vocab.json", "machine_vocab.json", "dataset.json", "inv_machine_vocab.json", "inv_human_vocab.json"]
 for filename in files:
     url = "https://github.com/LipeAma/Phrase2Date/raw/refs/heads/main/json_data/" + filename  # Local filename
     with requests.get(url, stream=True) as r:
@@ -42,6 +16,37 @@ for filename in files:
         globals()[filename[:-5]] = json.load(file)
     os.remove(filename)
 
-format2Date = partial(hotvec2phrase, vocab=inv_machine_vocab)
-formatDate = partial(date2hotvec, vocab=machine_vocab)
-formatPhrase = partial(phrase2hotvec, vocab=human_vocab)
+
+
+###################### Funções principais ######################
+def int2hotvec(intlist, vocab):
+  return array([[1 if j == val else 0 for j in range(len(vocab))] for val in intlist])
+
+def phrase2int(string, length, vocab=human_vocab):
+    string = string.replace(',', '')
+    string = string[:length]
+    string = string.lower()
+    unk = vocab['<unk>']
+    pad = vocab['<pad>']
+    int_list = [vocab.get(x, unk) for x in string]
+    padded_list = int_list + [pad] * (length - len(string))
+    return padded_list
+
+def date2int(string, vocab=machine_vocab):
+    return [vocab[x] for x in string]
+
+def hotvec2string(hotvec, vocab):
+    return [vocab[str(k)] for k in [n.argmax() for n in hotvec]]
+
+
+
+###################### Funções Parciais ######################
+def phrase2hotvec(string, length):
+    return int2hotvec(phrase2int(string, length), human_vocab)
+
+
+def date2hotvec(string):
+    return int2hotvec(date2int(string), machine_vocab)
+
+hotvec2phrase = partial(hotvec2string, vocab=inv_human_vocab)
+hotvec2date = partial(hotvec2string, vocab=inv_machine_vocab)
